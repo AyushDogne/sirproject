248c12c7faeb8559e0e82dae82b6e80f70640d4a
AyushDogne <ayushdogne2002@gmail.com>
2023-10-07 17:21:03 +0530

    only add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App(props) {
+//   console.log(props.color);
+  
+//   return (
+//     <div className="App">
+    
+//     <button onClick={props.clickEvent}> click</button>
+//     </div>
+//   );
+// }
+
+
+class App extends React.Component
+{
+  constructor(props)
+  {
+    super(props)
+  }
+  render()
+  {
+    console.log(this.props)
+    return  <button onClick={this.props.clickEvent}> click</button>  
+  }
+
 }
 
 export default App;

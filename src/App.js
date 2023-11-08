3ae8310e2d1064a01d92794e224a0220709602df
AyushDogne <ayushdogne2002@gmail.com>
2023-11-08 11:44:38 +0530

    compelete To-do list

--- src/App.js
+++ src/App.js
@@ -1,452 +1,235 @@
-// import logo from './logo.svg';
-import React, { useState } from 'react';
+// import { useState } from "react";
+//import { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
-// import React from 'react';
 
 
-//  class App extends React.Component
-//  {
- 
-//    constructor(props)
-//    {
-//      super(props)
-//      this.handleClick = this.handleClick.bind(this)
-//    }
-//     state = {todo: []}
-  
-//   handleClick(event)
-//   {
-//     event.preventDefault()
-//          console.log("handleClick is clicked")
-//         let newTodo = document.getElementById("addTodo").value
-//          // let newTodoObject = {
-//                 //   text: newTodo,
-//                 //   id: new Date().getTime(),
-//     // }
-//     console.log("Text readed: "+newTodo)
-//     // console.log(newTodoObject)
-//     // //console.log(this.state)
-//     this.state.todo.push(newTodo)
-//     console.log(this.state)
-//     document.getElementById("addTodo").value = ""
-//     //this.setState({todo:this.state.todo})
-//     //alert("clicked")
-//   }
-
-//   print()
-//   {
-//     console.log("Print")
-//     console.log(this.state)
-//   }
-//      render()
-//   {
-//     this.print()
-//     return <div>
-//       <h1>ToDo list</h1>
-//       <form onSubmit={this.handleClick}>
-//         <input type="text" id='addTodo'/>
-//        <button type='submit'>Add To-do</button>
-//       </form>
-//       {/* <div>
-//         <button type='submit'>Click me</button>
-//         {this.state.todo.map((tempTodo) =>
-//           <div className='todo' key={tempTodo.id}>
-//             <div className='todo-text'>{tempTodo.text}</div>
-//           </div>)         }       </div> */}
-//     </div>
-//   }
-
-// }
-//  export default App
-
-
-// class Ayush
-// {
-//     id 
-//     name
-//     contect
-//     constructor(id,name,contect)
-//     {
-//         this.id=id
-//         this.name=name
-//         this.contact=contect
-//     }
-//     print()
-//     {
-//         console.log(this.id)
-//         console.log(this.name)
-//         console.log(this.contect)
-//     }
-//     }
-
-// class Pooja extends Ayush
-// {
-//     addrres
-//     constructor(id,name,contect,addrres)
-//     {
-//         this.addrres=addrres
-//         super(id,name,contect,addrres)
-
-//     }
-//    pritff()
-//    {
-//     super.print();
-//     console.log(this.addrres)
-   
-//    }
-
-// }
-
-// let dogne=new Pooja(1,"ayush",76543,"khandwa")
-// dogne.pritff()
-
-// }
- 
-
-// function App(props) {
-//   console.log(props.color);
-  
-//   return (
-//     <div className="App">
-    
-//     <button onClick={props.event}> click</button>
-    
-//     </div>
-//   );
-// }
-
-
-// class App extends React.Component
-// {
-  
-//   constructor(props)
-//   {
-//     super(props)
-//   }
-//   render()
-//   {
-//     return <button onClick={this.props.event}>click</button>
-//   }
-// }
-
-
-
-// class App extends React.Component
-// { 
-//   constructor(props)
-//   {
-//     super(props)
-//   }
-//   state = {counter: 0}
-//    incrementCounter = () =>
-//    {
-//    this.setState({counter:parseInt(this.state.counter)+1})
-//    }
-//
-//   render()
-//   {
-//     return(
-//       <div> <button onClick={this.incrementCounter}>click</button>
-//     counter:{this.state.counter}
-//     </div>
-//     )
-//   }
-//
-// }
-
-
-// class App extends React.Component
-// {
-//   constructor(props)
-//   {
-//     super(props)
-//     
-//   }
-  
-//   static getDerivedStateFromProps()
-//   
-//   state={counter:0}
-//   intcount=()=>
-//   {
-//     this.setState({counter:(this.state.counter)+2})
-//   }
-//   render()
-  
-//   {
-//  
-//     return(
-//       <div>
-//      <button onClick={this.intcount}>click kar </button>
-//        counter:{this.state.counter}
-//       </div>
-//     )
-//   }
-
-// }
-
-
-
-// class InnerComponent extends React.Component
-// {
-//   render()
-//   {
-//     return <div>Inner component is here</div>
-//   }
-
-//   componentDidMount()
-//   {
-//     console.log("####Mounting InnerComponent####")
-//   }
-
-//   componentWillUnmount()
-//   {
-//     console.log("****Unmounting InnerComponent****")
-//   }
-
-// }
-
-// class App extends React.Component
-// {
-//   state = {counter:0, innerComponent:<InnerComponent/>}
-//   constructor(props)
-//   {
-//     super(props)
-//     console.log("I am inside Constructor")
-//   }
-
-//   static getDerivedStateFromProps()
-//   {
-//     console.log("I am inside getDerivedStateFromProps")
-//     return true
-//   }
-
-//   shouldComponentUpdate()
-//   {
-//     console.log("Inside shouldComponentUpdate")
-//     return true
-//   }
-
-//   incrementCounter = () => 
-//   {
-//     //updating state on function call
-//     this.setState({counter:this.state.counter+1})
-//     if(this.state.counter % 2 === 0)
-//     {
-//       this.setState({innerComponent:<InnerComponent/>})
-//     }
-//     else
-//     {
-//       this.setState({innerComponent:<div>Unmounted</div>})
-//     }
-    
-//   }
-
-//   // Overridden render function
-//   render()
-//   {
-//     console.log("Inside the render method")
-//     console.log(this.props)
-//     return (
-//         <div>
-//           <button onClick={this.incrementCounter}>Click me</button>
-//           <br/>
-//           Counter:{this.state.counter}
-//           <br/>
-//           <div>{this.state.innerComponent}</div>
-//         </div>
-//     )
-//   }
-
-//   getSnapshotBeforeUpdate(prevProps, prevState)
-//   {
-//     console.log("Inside getSnapshotBeforeUpdate")
-//     console.log("Previous Prop:")
-//     console.log(prevProps)
-//     console.log("Previous State:")
-//     console.log(prevState)
-//     console.log("--------------------")
-//     console.log("Updated Prop:")
-//     console.log(this.props)
-//     console.log("Update State:")
-//     console.log(this.state)
-//     return true
-//   }
-
-//   componentDidMount()
-//   {
-//     console.log("I am inside componentDidMount")
-//   }
-// }
-
-
-// class App extends React.Component
-// {
-//     constructor(props)
-//     {
-//         super(props)
-//     }
-//     state={counter:0}
-//     componentDidMount()
-//     {
-//      console.log("the render method call first time");
-//     }
-//     ints=()=>
-//     {
-//         this.setState({counter:(this.state.counter)+1})
-//     }
-
-// componentDidUpdate(prevProps,prevState)
-// {
-//     console.log(prevProps)
-//     console.log(prevState.counter)
-
-//     if(prevProps!==prevState)
-//     {
-//         console.log("component updeted")
-//     }
-// }
-
-//     render()
-//     {
-//         return(
-//             <>
-//             <h1>{this.state.counter}</h1>
-//                 <button onClick={this.ints}>haooomre lala</button>
-//             </>
-//         )
-//     }
-// }
-
-
-
-
-
-// const App = () =>
-// {
-//   let [todo, setTodo] = useState([]);
-
-//   function handleClick(event)
-//   {
-//     event.preventDefault()
-//     console.log("-------------------handleClick is clicked-------------------")
-//     let newTodo = document.getElementById("addTodo").value
-//     let newTodoObject = {
-//       id: todo.length,
-//       text: newTodo,
-//       completed: false,
-//     }
-//     //console.log("Text readed: "+newTodo)
-//     // console.log(newTodoObject)
-//     //console.log(todo)
-//     todo.push(newTodoObject)
-//     //setTodo([...todo].concat(newTodoObject))
-//     setTodo([...todo])
-//     console.log(todo)
-//     document.getElementById("addTodo").value = ""
-//   }
-
-//   function deleteTodo(id)
-//   {
-//     //event.preventDefault()
-//     console.log("-------------------Delete button called!-------------------")
-//     console.log(todo)
-//     let udatedTodo = todo.filter((tempTodo)=>  tempTodo.id !== id)
-//     console.log("Update Array: "+udatedTodo)
-//     setTodo(udatedTodo)
-//     console.log(todo)
-//   }
-  
-//   return <div>
-//        <h1>ToDo list</h1>
-//        <form onSubmit={handleClick}>
-//          <input type="text" id='addTodo'/>
-//          <button type='submit'>Add To-do</button>
-//        </form>
-//        <div>
-//           {todo.map((tempTodo) =>
-//             {
-//               return <div id="todoListID">
-//                 <div id="todoTextId">{tempTodo.text}</div>
-//                 <button onClick={()=>deleteTodo(tempTodo.id)}>Delete</button>
-//                 </div>
-//             })
-//           }          
-//       </div>
-//     </div>
-
-//  }
-
-
-
-
-let App=()=>
+const App=()=>
 {
-  let [todo, settodo]=useState([])
-  
-  function hendaltodo(event)
+ const [todo,setTodo]=useState([])
+ const [editingFlag,setediTing]=useState(-1)
+
+
+function addTodo(evant)
+{
+  console.log("------addTodo-------");
+  let tempTodo=document.getElementById("todoInput").value
+  console.log("tempTodo: "+tempTodo)
+
+if(todo.length>0)
+{
+addToarray(todo[todo.length-1].id+1,tempTodo, false)
+}
+else
+{
+addToarray(0,tempTodo, false)
+}
+}
+function addToarray( id,text,completed)
+{
+
+  let tempTodoObject=
   {
-    event.preventDefault()
-    console.log("----------runn--------");
-    let newtodo=document.getElementById("addtodo").value
-    let newtodoobject=
-    {
-      id:todo.length,
-      text:newtodo,
-    }
-    todo.push(newtodoobject)
-    console.log(todo)
-    settodo([...todo])
-    document.getElementById("addtodo").value=""
+    id:id,
+    text:text,
+    completed:completed
+
   }
-    function deletetodo(id)
-    {
-      console.log("------runn delete---------")
-      let updatetodo=todo.filter((temptodo)=>temptodo.id!==id)
-      settodo(updatetodo)
-      console.log(updatetodo)
-
-    } 
-  
-  return(
-    <div className='mandiv'>
-      <h1 className='ss'>ADD TODO LIST</h1>
-      <form onSubmit={hendaltodo}>
-        <input type="text" placeholder='Enter any list' id='addtodo'/>
-        <input type="submit" id='style' />
-      </form>
-      
-       <div>
-        {todo.map((temptodo)=>
-        {
-          return(
-          <>
-            <div className='ss'>{temptodo.text}</div>
-          <button onClick={()=>deletetodo(temptodo.id)} id='style'>delete</button>
-          </>
-          )
-        })}
-       </div>
-
-
-    </div>
-  )
+  todo.push(tempTodoObject)
+  console.log(todo)
+  setTodo([...todo])
+  document.getElementById("todoInput").value = ""
 }
 
+function deleteTodo(id)
+{
+  console.log("-----delete function is running-----")
+  let deleteTodo=todo.filter((deleteTodo)=>deleteTodo.id !==id)
+  setTodo([...deleteTodo])
+  console.log(deleteTodo)
+
+}
+
+function mockbutton()
+{
+  if(todo.length>0)
+  {
+  console.log("mock is rum")
+ addToarray(todo[todo.length-1].id+1,"todo:1",true)
+ addToarray(todo[todo.length-1].id+1,"todo:2",false)
+ addToarray(todo[todo.length-1].id+1,"todo:3",false)
+ addToarray(todo[todo.length-1].id+1,"todo:4",false)
+}
+else
+{
+  addToarray(0,"todo:1",true)
+  addToarray(todo[todo.length-1].id+1,"todo:2",false)
+  addToarray(todo[todo.length-1].id+1,"todo:3",false)
+  addToarray(todo[todo.length-1].id+1,"todo:4",false)
+
+}
+}
+function checkLitener(id)
+{
+  console.log("-------check-------")
+  todo.map(element=>{ 
+    if(element.id==id)
+    {
+      element.completed=!element.completed
+    }
+    return element
+  })
+  console.log(todo)
+  setTodo([...todo])
+}
+function editTodo(id)
+{
+  console.log("-----edit todo-------")
+  setediTing(id)
+
+}
+
+function upDateTodo()
+{
+  console.log("-----eun upDateTodo------")
+  console.log("editingFlag: "+editingFlag)
+
+  let tempTodo=todo.map(element=>{
+
+      if(element.id==editingFlag)
+      {
+        element.text=document.getElementById("editTodo").value
+      }
+      return element
+  })
+  setediTing(-1)
+  setTodo([...tempTodo])
+}
+
+  return <div>
+    <h1>To-do Application</h1>
+    <button onClick={()=>mockbutton()}>mock</button>
+    <input type="text" placeholder="Enter to-do here" id="todoInput" />
+    <button onClick={()=>addTodo()}>Add to-do</button>
+
+   {
+      todo.map(element =>
+     {
+      return <div>
+      {
+
+        //compeleted todo
+        element.completed ?
+        <div>
+        <input type="checkbox" onChange={()=>checkLitener(element.id)}checked />
+        <s>{element.text+"    "}</s>
 
 
+      </div>:
+      //incompeleted todo
+      (element.id == editingFlag ? 
+
+        <div>
+        <input type="checkbox" onClick={()=>checkLitener(element.id)}/>
+        <input type="text" Value={element.text+"    "} placeholder="updating todo" id="editTodo" />
+
+      <button onClick={()=>deleteTodo(element.id)}>Delete todo</button>
+      <button onClick={()=>upDateTodo()}>seve to-do</button>
+
+
+      </div>:
+
+      <div>
+        <input type="checkbox" onClick  ={()=>checkLitener(element.id)}/>
+      {element.text+"    "}
+      <button onClick={()=>deleteTodo(element.id)}>Delete todo</button>
+      <button onClick={()=>editTodo(element.id)}>Edit</button>
+
+      </div>
+      )
+
+
+      }
+      </div>
+    })
+  }
+  </div>
+}
 
 
 export default App;
 
+// import React from 'react';
+
+// const App = () => {
+//   let [todo, setTodo] = useState([])
+//   function AddTodo(event) {
+//     console.log("---------Todo list added--------")
+//     let newTodo = document.getElementById("inputTodo").value
+//     console.log(newTodo)
+//     Arraytodo(todo.length,newTodo)
+//   }
+  
+//   function deleteTodo(id) 
+
+//   {
+//     console.log("-------delete todo------")
+//     let delet = todo.filter((element) => element.id != id)
+//     setTodo([...delet])
+//     console.log(delet)
+     
+//   }
+
+//   function Arraytodo(id,text)
+//   {
+//     let upDatetodo =
+//     {
+//       id:id,
+//       text:text,
+
+//     }
+//     todo.push(upDatetodo)
+
+//     setTodo([...todo])
+//     console.log(todo)
+//   }
+//   function mockButton()
+//    {
+//    Arraytodo (todo.length,"todo-3")
+//    Arraytodo(todo.length,"todo-2")
+//    Arraytodo (todo.length,"todo-4")
+//    Arraytodo(todo.length,"todo-1")
+//    Arraytodo (todo.length,"todo-5")
+//   }
+  
+//   function editTodo()
+//   {
+
+//   }
+
+//   return <div>
+//     <h1>Add todo list</h1>
+//     <button onClick={() => mockButton()}>mock</button>
+//     <input type="text" placeholder='Enter todo list' id='inputTodo' />
+//     <button onClick={() => AddTodo()}>Add todo-list</button>
 
 
+//     {todo.map((element) =>
+//      {
+//       return <div>
+//         <div>{element.text}</div>
+//         <button onClick={() => deleteTodo(element.id)}>Delete-todo</button>
+//         <button onClick={()=>editTodo()}>Edit</button>
 
+//       </div>
 
+//     }
+//     )}
 
+//   </div>
+// }
 
-
-
-
+// export default App
 
 
 
